1976ce36cf4a664e662f926e6ac3e0a1d0fbc329
Antonio <abamorim36@gmail.com>
2024-03-24 08:33:30 -0300

    Update Todo Component

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -4,12 +4,14 @@ import { Trash } from 'phosphor-react';
 
 export function Todo(){
      return(
-          <div className={styles.todo}>
-               <input type="radio"></input>
-               <p>Estudar Reactjs</p>
-               <button title="Deletar todo">
-                    <Trash size={24} color="#9a9996" />
-               </button>
+          <div className={styles.todoCard}>
+               <div className={styles.todo}>
+                    <input type="radio"></input>
+                    <p>Estudar Reactjs</p>
+                    <button title="Deletar todo">
+                         <Trash size={24} />
+                    </button>
+               </div>
           </div>
      )
 }
