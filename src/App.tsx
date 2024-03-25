b0f20ad20645d34fc2224f6e6f35c1bccd2038fa
Antonio <abamorim36@gmail.com>
2024-03-25 08:26:48 -0300

    Creating Todo Array

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,36 @@ import { Todo } from './components/Todo.tsx'
 
 import './global.css'
 
+const todos = [
+  {
+       id: 1,
+       isComplete: false,
+       content: [
+            {type: 'parapraph', 
+            content: 'Estudar Reactjs'}
+       ]    
+  },
+  {
+        id: 2,
+        isComplete: false,
+        content: [
+            {type: 'parapraph', 
+            content: 'Estudar Reactjs'}
+        ]    
+  }
+];
+
 export function App() {
   return (
     <>
       <Header />
       <AddBar />
       <TaskList />
-      <Todo />
+      <main>
+        {todos.map(todo => {
+          return <Todo />
+        })}
+      </main>
     </>
   )
 }
